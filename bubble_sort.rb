deba284bee6abd6c13a67b4c1642568b52f2a056
Ricardo <ricardomg92@hotmail.com>
2022-01-15 19:05:30 +0100

    correctly sorts the array

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,17 @@
 def bubble_sort(arr)
+  (arr.length - 1).times do
+    i = 1
+    (arr.length - 1).times do
+      if arr[i - 1] > arr[i]
+        number1 = arr[i - 1]
+        number2 = arr[i]
+        arr[i - 1] = number2
+        arr[i] = number1
+      end
+      i += 1
+    end
+  end
   p arr
 end
 
-bubble_sort([4,3,78,2,0,2])
+bubble_sort([4, 3, 78, 2, 0, 2])
